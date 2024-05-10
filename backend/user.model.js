655fa0c16505b4450a92727dc9ba84e28c982e7c
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-11 01:41:01 +0530

    update the user model

--- backend/user.model.js
+++ backend/user.model.js
@@ -82,6 +82,4 @@ userSchema.methods.generateRefreshToken = function () {
   };
   
 
-userSchema.methods.generateRefreshToken = function () {};
-
 export const User = mongoose.model("User", userSchema);
