64c2b4c72b1bd43b4a23721a06de28262f22e9be
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-11 16:03:16 +0530

    add userController , userRoute and setup all configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,5 +23,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration 
+app.use("/api/v1/users/", userRouter)
+
+// http://localhost:4000/api/v1/users/register
+
+
 
 export default app; 
