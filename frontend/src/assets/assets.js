c3327b081231fe78be4a695ccb76ef553beb6424
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-31 12:35:46 +0530

    update the hero component

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -50,7 +50,7 @@ export const course_list = [
 export const video_list = [
   {
     _id: 1,
-    name: "React With JavaScript Mastery",
+    name: "React With JavaScript",
     category: "Frontend",
     subcategory: "React",
     url: "https://youtu.be/F627pKNUCVQ?si=PjDKCMDU1kzd2IJ9"
@@ -60,231 +60,231 @@ export const video_list = [
     name: "Advanced Angular",
     category: "Frontend",
     subcategory: "Angular",
-    url: "https://www.example.com/advanced-angular"
+    url: "https://youtu.be/f7BJFTEbc10?si=xv3m9JRHGJbucGc7"
   },
   {
     _id: 3,
-    name: "Vue.js Fundamentals",
+    name: "Vue.js Course",
     category: "Frontend",
     subcategory: "Vue.js",
-    url: "https://www.example.com/vuejs-fundamentals"
+    url: "https://youtu.be/1GNsWa_EZdw?si=roDeFswgMhZtOo_G"
   },
   {
     _id: 4,
-    name: "Node.js and MongoDB",
+    name: "Node.js & Express.js",
     category: "Backend",
     subcategory: "Node.js",
-    url: "https://www.example.com/nodejs-mongodb"
+    url: "https://youtu.be/ekRpc5YgVZU?si=hau8Z-Ifw1aFtXId"
   },
   {
     _id: 5,
     name: "Backend Development with Java",
     category: "Backend",
     subcategory: "Java",
-    url: "https://www.example.com/backend-java"
+    url: "https://youtu.be/Uh-N_6Lccr4?si=BQHCGvAYCiOPbYsW"
   },
   {
     _id: 6,
-    name: "Docker Essentials",
+    name: "Docker Course",
     category: "DevOps",
     subcategory: "Docker",
-    url: "https://www.example.com/docker-essentials"
+    url: "https://youtu.be/OhnTMWmfTBE?si=hrU7qjpAClA8aLQq"
   },
   {
     _id: 7,
-    name: "Kubernetes Basics",
+    name: "Kubernetes Course",
     category: "DevOps",
     subcategory: "Kubernetes",
-    url: "https://www.example.com/kubernetes-basics"
+    url: "https://youtu.be/rBeyHDKLVqM?si=KWR32NldOBOoQ-IW"
   },
   {
     _id: 8,
-    name: "Terraform Introduction",
+    name: "Terraform Course",
     category: "DevOps",
     subcategory: "Terraform",
-    url: "https://www.example.com/terraform-introduction"
+    url: "https://youtu.be/dX-sbUs9Sk0?si=p_OUpxHJn3KZAD42"
   },
   {
     _id: 9,
-    name: "Blockchain Development with Ethereum",
+    name: "Blockchain Development",
     category: "Blockchain",
     subcategory: "Ethereum",
-    url: "https://www.example.com/blockchain-ethereum"
+    url: "https://youtu.be/Wn_Kb3MR_cU?si=IQlBOu7EXR7gwwD2"
   },
   {
     _id: 10,
-    name: "Machine Learning with Python",
+    name: "Python Full Course",
     category: "Machine Learning",
     subcategory: "Python",
-    url: "https://www.example.com/ml-python"
+    url: "https://youtu.be/-hh6raHUUvk?si=VLqCtjt0OJT_B7F2"
   },
   {
     _id: 11,
-    name: "AWS Cloud Engineering",
+    name: "AWS Course",
     category: "Cloud Engineering",
     subcategory: "AWS",
-    url: "https://www.example.com/aws-cloud-engineering"
+    url: "https://youtu.be/V04FvpXSFS8?si=0ja-b0tRQBXeR0GI"
   },
   {
     _id: 12,
-    name: "React Advanced Patterns",
+    name: "React Advanced Course",
     category: "Frontend",
     subcategory: "React",
-    url: "https://www.example.com/react-advanced-patterns"
+    url: "https://youtu.be/lAFbKzO-fss?si=2MU3mxRXtDrcEjdp"
   },
-  {
-    _id: 13,
-    name: "Angular Reactive Forms",
-    category: "Frontend",
-    subcategory: "Angular",
-    url: "https://www.example.com/angular-reactive-forms"
-  },
-  {
-    _id: 14,
-    name: "Vue.js State Management",
-    category: "Frontend",
-    subcategory: "Vue.js",
-    url: "https://www.example.com/vuejs-state-management"
-  },
-  {
-    _id: 15,
-    name: "Express.js and MongoDB",
-    category: "Backend",
-    subcategory: "Node.js",
-    url: "https://www.example.com/expressjs-mongodb"
-  },
-  {
-    _id: 16,
-    name: "Spring Boot and Microservices",
-    category: "Backend",
-    subcategory: "Java",
-    url: "https://www.example.com/spring-boot-microservices"
-  },
-  {
-    _id: 17,
-    name: "Advanced Docker",
-    category: "DevOps",
-    subcategory: "Docker",
-    url: "https://www.example.com/advanced-docker"
-  },
-  {
-    _id: 18,
-    name: "Kubernetes Deep Dive",
-    category: "DevOps",
-    subcategory: "Kubernetes",
-    url: "https://www.example.com/kubernetes-deep-dive"
-  },
-  {
-    _id: 19,
-    name: "Terraform for AWS",
-    category: "DevOps",
-    subcategory: "Terraform",
-    url: "https://www.example.com/terraform-aws"
-  },
-  {
-    _id: 20,
-    name: "Solidity Smart Contracts",
-    category: "Blockchain",
-    subcategory: "Ethereum",
-    url: "https://www.example.com/solidity-smart-contracts"
-  },
-  {
-    _id: 21,
-    name: "Deep Learning with TensorFlow",
-    category: "Machine Learning",
-    subcategory: "TensorFlow",
-    url: "https://www.example.com/deep-learning-tensorflow"
-  },
-  {
-    _id: 22,
-    name: "Azure Cloud Engineering",
-    category: "Cloud Engineering",
-    subcategory: "Azure",
-    url: "https://www.example.com/azure-cloud-engineering"
-  },
-  {
-    _id: 23,
-    name: "React Hooks in Depth",
-    category: "Frontend",
-    subcategory: "React",
-    url: "https://www.example.com/react-hooks-depth"
-  },
-  {
-    _id: 24,
-    name: "Angular Material Design",
-    category: "Frontend",
-    subcategory: "Angular",
-    url: "https://www.example.com/angular-material-design"
-  },
-  {
-    _id: 25,
-    name: "Vue.js with TypeScript",
-    category: "Frontend",
-    subcategory: "Vue.js",
-    url: "https://www.example.com/vuejs-typescript"
-  },
-  {
-    _id: 26,
-    name: "Node.js Performance Tuning",
-    category: "Backend",
-    subcategory: "Node.js",
-    url: "https://www.example.com/nodejs-performance-tuning"
-  },
-  {
-    _id: 27,
-    name: "Java Concurrency",
-    category: "Backend",
-    subcategory: "Java",
-    url: "https://www.example.com/java-concurrency"
-  },
-  {
-    _id: 28,
-    name: "CI/CD with Jenkins and Docker",
-    category: "DevOps",
-    subcategory: "Docker",
-    url: "https://www.example.com/cicd"
-  },
-  {
-    _id: 29,
-    name: "Power BI with john dev",
-    category: "Data Science",
-    subcategory: "Power BI",
-    url: "https://www.example.com/cicd"
-  },
-  {
-    _id: 30,
-    name: "C++ with john dev",
-    category: "Programming Language",
-    subcategory: "C++",
-    url: "https://www.example.com/cicd"
-  },
-  {
-    _id: 31,
-    name: "Python with john dev",
-    category: "Programming Language",
-    subcategory: "Python",
-    url: "https://www.example.com/cicd"
-  },
-  {
-    _id: 32,
-    name: "Solidity with john dev",
-    category: "Programming Language",
-    subcategory: "Solidity",
-    url: "https://www.example.com/cicd"
-  },
-  {
-    _id: 33,
-    name: "Java with john dev",
-    category: "Programming Language",
-    subcategory: "Java",
-    url: "https://www.example.com/cicd"
-  },
-  {
-    _id: 34,
-    name: "JavaScript with john dev",
-    category: "Programming Language",
-    subcategory: "JavaScript",
-    url: "https://www.example.com/cicd"
-  },
+  // {
+  //   _id: 13,
+  //   name: "Angular Course",
+  //   category: "Frontend",
+  //   subcategory: "Angular",
+  //   url: "https://www.example.com/angular-reactive-forms"
+  // },
+  // {
+  //   _id: 14,
+  //   name: "Vue.js State Management",
+  //   category: "Frontend",
+  //   subcategory: "Vue.js",
+  //   url: "https://www.example.com/vuejs-state-management"
+  // },
+  // {
+  //   _id: 15,
+  //   name: "Express.js and MongoDB",
+  //   category: "Backend",
+  //   subcategory: "Node.js",
+  //   url: "https://www.example.com/expressjs-mongodb"
+  // },
+  // {
+  //   _id: 16,
+  //   name: "Spring Boot and Microservices",
+  //   category: "Backend",
+  //   subcategory: "Java",
+  //   url: "https://www.example.com/spring-boot-microservices"
+  // },
+  // {
+  //   _id: 17,
+  //   name: "Advanced Docker",
+  //   category: "DevOps",
+  //   subcategory: "Docker",
+  //   url: "https://www.example.com/advanced-docker"
+  // },
+  // {
+  //   _id: 18,
+  //   name: "Kubernetes Deep Dive",
+  //   category: "DevOps",
+  //   subcategory: "Kubernetes",
+  //   url: "https://www.example.com/kubernetes-deep-dive"
+  // },
+  // {
+  //   _id: 19,
+  //   name: "Terraform for AWS",
+  //   category: "DevOps",
+  //   subcategory: "Terraform",
+  //   url: "https://www.example.com/terraform-aws"
+  // },
+  // {
+  //   _id: 20,
+  //   name: "Solidity Smart Contracts",
+  //   category: "Blockchain",
+  //   subcategory: "Ethereum",
+  //   url: "https://www.example.com/solidity-smart-contracts"
+  // },
+  // {
+  //   _id: 21,
+  //   name: "Deep Learning with TensorFlow",
+  //   category: "Machine Learning",
+  //   subcategory: "TensorFlow",
+  //   url: "https://www.example.com/deep-learning-tensorflow"
+  // },
+  // {
+  //   _id: 22,
+  //   name: "Azure Cloud Engineering",
+  //   category: "Cloud Engineering",
+  //   subcategory: "Azure",
+  //   url: "https://www.example.com/azure-cloud-engineering"
+  // },
+  // {
+  //   _id: 23,
+  //   name: "React Hooks in Depth",
+  //   category: "Frontend",
+  //   subcategory: "React",
+  //   url: "https://www.example.com/react-hooks-depth"
+  // },
+  // {
+  //   _id: 24,
+  //   name: "Angular Material Design",
+  //   category: "Frontend",
+  //   subcategory: "Angular",
+  //   url: "https://www.example.com/angular-material-design"
+  // },
+  // {
+  //   _id: 25,
+  //   name: "Vue.js with TypeScript",
+  //   category: "Frontend",
+  //   subcategory: "Vue.js",
+  //   url: "https://www.example.com/vuejs-typescript"
+  // },
+  // {
+  //   _id: 26,
+  //   name: "Node.js Performance Tuning",
+  //   category: "Backend",
+  //   subcategory: "Node.js",
+  //   url: "https://www.example.com/nodejs-performance-tuning"
+  // },
+  // {
+  //   _id: 27,
+  //   name: "Java Concurrency",
+  //   category: "Backend",
+  //   subcategory: "Java",
+  //   url: "https://www.example.com/java-concurrency"
+  // },
+  // {
+  //   _id: 28,
+  //   name: "CI/CD with Jenkins and Docker",
+  //   category: "DevOps",
+  //   subcategory: "Docker",
+  //   url: "https://www.example.com/cicd"
+  // },
+  // {
+  //   _id: 29,
+  //   name: "Power BI with john dev",
+  //   category: "Data Science",
+  //   subcategory: "Power BI",
+  //   url: "https://www.example.com/cicd"
+  // },
+  // {
+  //   _id: 30,
+  //   name: "C++ with john dev",
+  //   category: "Programming Language",
+  //   subcategory: "C++",
+  //   url: "https://www.example.com/cicd"
+  // },
+  // {
+  //   _id: 31,
+  //   name: "Python with john dev",
+  //   category: "Programming Language",
+  //   subcategory: "Python",
+  //   url: "https://www.example.com/cicd"
+  // },
+  // {
+  //   _id: 32,
+  //   name: "Solidity with john dev",
+  //   category: "Programming Language",
+  //   subcategory: "Solidity",
+  //   url: "https://www.example.com/cicd"
+  // },
+  // {
+  //   _id: 33,
+  //   name: "Java with john dev",
+  //   category: "Programming Language",
+  //   subcategory: "Java",
+  //   url: "https://www.example.com/cicd"
+  // },
+  // {
+  //   _id: 34,
+  //   name: "JavaScript with john dev",
+  //   category: "Programming Language",
+  //   subcategory: "JavaScript",
+  //   url: "https://www.example.com/cicd"
+  // },
 
 ]
--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,35 +1,4 @@
 
-// import { Link } from "react-router-dom";
-// import hero from "../../assets/hero.png";
-
-// const Hero = () => {
-//   return (
-//     <div className="relative h-screen flex items-center justify-center">
-//       <div
-//         className="absolute inset-0 bg-cover bg-center"
-//         style={{ backgroundImage: `url(${hero})` }}
-//       >
-//         <div className="absolute inset-0 bg-gradient-to-r 
-//          from-slate-700 via-slate-700 to-slate-700 opacity-90 blur-lg">
-
-//          </div>
-//       </div>
-//       <div className="relative z-10 text-center text-white">
-//         <h1 className="text-5xl font-bold mb-4">Welcome to Tech World</h1>
-//         <p className="text-xl mb-8">We create amazing tech community</p>
-//         <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-lg text-lg">
-//          <Link to="/courses"> Get Started</Link>
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-
-
-
 
 import { Link } from "react-router-dom";
 import hero from "../../assets/hero.png";
@@ -47,7 +16,7 @@ const Hero = () => {
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
           Welcome to Tech World
         </h1>
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-8">
+        <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-8 font-mono  ">
           We create amazing tech community
         </p>
         <button className="px-4 py-2 sm:px-6 sm:py-3 bg-blue-600 hover:bg-blue-700 rounded-lg text-base sm:text-lg">
